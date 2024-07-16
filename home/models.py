b551ff6dda317ee994b875e6a4e616bc10868310
Elias TOURNIER CORRIGNAN <elias.tournier@gmail.com>
2024-07-16 19:36:03 +0200

    Real Initial Commit

---
+++ home/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+
+# import MultiFieldPanel:
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel
+
+
+class HomePage(Page):
+    # add the Hero section of HomePage:
+    image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+        help_text="Homepage image",
+    )
+    hero_text = models.CharField(
+        blank=True,
+        max_length=255, help_text="Write an introduction for the site"
+    )
+
+    body = RichTextField(blank=True)
+
+    # modify your content_panels:
+    content_panels = Page.content_panels + [
+        MultiFieldPanel(
+            [
+                FieldPanel("image"),
+                FieldPanel("hero_text"),
+            ],
+            heading="Hero section",
+        ),
+        FieldPanel('body'),
+    ]
