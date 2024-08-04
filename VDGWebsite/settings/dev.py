83c558a80d7509821a87a5d16e9184c254637d6f
Elias TOURNIER CORRIGNAN <elias.tournier@gmail.com>
2024-08-04 23:58:00 +0200

    Modifying base django template + replacing SECRET_KEY

--- VDGWebsite/settings/dev.py
+++ VDGWebsite/settings/dev.py
@@ -1,16 +1,16 @@
 from .base import *
+import os
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$5(#3(y1ti654o5ipvso$q-zjwlpqjl070olq3j&iy0hbaddjy"
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-$5(#3(y1ti654o5ipvso$q-zjwlpqjl070olq3j&iy0hbaddjy")
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
 
 try:
     from .local import *
